b0ae7f334151b34d0dbffb5c664cb0e8385caed0
default233 <1149863397@qq.com>
2021-03-23 22:18:39 +0800

    login register

--- student/src/main/java/com/chen/student/StudentApplication.java
+++ student/src/main/java/com/chen/student/StudentApplication.java
@@ -1,9 +1,11 @@
 package com.chen.student;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.chen.biz", "com.chen.student"})
+@MapperScan(basePackages = {"com.chen.biz.mapper"})
 public class StudentApplication {
 
     public static void main(String[] args) {
